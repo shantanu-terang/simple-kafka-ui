7be9951ade2e17093a887283112af7aa09a8cfac
Shantanu Terang <shantanuterang@gmail.com>
2025-08-17 13:35:52 +0530

    Dev - fixed acidentally removed code

--- src/backend/src/app.js
+++ src/backend/src/app.js
@@ -1,8 +1,11 @@
 const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
 const { apiRouter } = require('./routes/api')
 const { SocketHandler } = require('./controllers/socket/socket')
 const { app, io, server } = require('./setup')
+
+app.use(bodyParser.json())
 
 // this will autlmatically load the public/index.html for / route
 app.use(express.static(path.join(process.cwd(), 'public')));
